ddacfb2798af615f80610b97366b1a1df83222ce
atecle <adam.tecle@gc.com>
2018-04-06 13:10:42 -0400

    shpotify aliases

---
+++ shpotify/shpotify.zsh
@@ -0,0 +1,9 @@
+# v important 
+alias sprev='spotify prev'
+alias spnext='spotify next'
+alias spinfo='spotify info'
+alias spause='spotify pause' 
+alias splay='spotify play'
+alias sprplay='spotify replay'
+alias sptog='spotify toggle'
+
